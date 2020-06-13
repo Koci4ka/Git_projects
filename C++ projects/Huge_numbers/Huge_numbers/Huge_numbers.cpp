e05977406391e4ac74b80d1930590e93b0d724e4
Julia <dlya.peregovorov@yandex.ru>
2020-06-13 22:31:57 +0300

    Huge numbers second version(methods)

--- C++ projects/Huge_numbers/Huge_numbers/Huge_numbers.cpp
+++ C++ projects/Huge_numbers/Huge_numbers/Huge_numbers.cpp
@@ -1,93 +1,167 @@
 #include <iostream>
 #include <string>
+#include <locale>
+#include <windows.h>
 using namespace std;
 
 class HugeNumbers {
 public:
-	HugeNumbers(string given_number1 = "", string given_number2 = "")
+	HugeNumbers(string given_number = "0")
 	{
-		first_number = given_number1;
-		second_number = given_number2;
+		int size;
+		int string_size = given_number.size();
+		bool wasSign = true;
+		if (given_number[0] == '+') {
+			isPositive = true;
+			size = given_number.size() / 2;
+		}
+		else if (given_number[0] == '-') {
+			isPositive = false;
+
+			size = given_number.size() / 2;
+		}
+		else {
+			isPositive = true;
+			size = (given_number.size() + 1) / 2;
+			wasSign = false;
+		}
+
+		number_get = new unsigned char[size];
+
+		if (wasSign) {
+			string_size -= 1;
+		}
+
+		for (int i = 0; i < size; i++) { 
+			int c1 = 0, c2 = 0;
+			if (i == (size - 1) && (string_size % 2 != 0)) {
+				c2 = given_number[given_number.size() - 1 - 2 * i] - '0';
+			}
+			else {
+				c1 = given_number[given_number.size() - 2 - 2 * i] - '0';
+				c2 = given_number[given_number.size() - 1 - 2 * i] - '0';
+			}
+			number_get[i] = c1 * 10 + c2;
+		}
 	}
+
+	
+	/*ввывести число
+	знак числа
+	сложение вычитание ---  HugeNumbers Add(HugeNumbers number_2)  
+			складываем намб1(наш класс) + намб2(его передали) = намб3 его возвращаем
+
+	*/
 
 	~HugeNumbers() {}
 
-	bool foundSign1(string first_number) {}
-	bool foundSign2(string second_number) {}
-	string getSum(string first_number, string second_number) {}
-	string getSubtraction (string first_number, string second_number) {}
+	void showNumber(string given_number) const {
+		int string_size = given_number.size();
+		for (int i = 0; i < size; i++) {
+			int c1 = 0, c2 = 0;
+			if (i == (size - 1) && (string_size % 2 != 0)) {
+				c2 = given_number[given_number.size() - 1 - 2 * i] - '0';
+			}
+			else {
+				c1 = given_number[given_number.size() - 2 - 2 * i] - '0';
+				c2 = given_number[given_number.size() - 1 - 2 * i] - '0';
+			}
+			number_get[i] = c1 * 10 + c2;
+		}
+		for (int i = 0; i < size; i++) {
+			cout << int(number_get[i]);
+		};
+	}
+
+	bool getSign(string given_number) {
+		if (given_number[0] == '+') {
+			return true;
+		}
+		else if (given_number[0] == '-') {
+			return false;
+		}
+		else {
+			return true;
+
+		}
+	};
+	HugeNumbers Add(string given_number) {
+		return given_number;
+	};
+
 	
 
 private: 
-	string first_number;
-	string second_number;
 
+	int size;
+	bool isPositive;
+	unsigned char *number_get;
 };
 
 
-int main()
-{
-	setlocale(LC_ALL, "Russian");
-	HugeNumbers numbers;
-	string number1;
-	string number2;
-	int value = 0;
-	bool result;
 
-	cout << "Операции, которые можно совершать: " << endl;
-	cout << "1 - Вычислить сумму чисел  " << endl;
-	cout << "2 - Вычислить разность чисел" << endl;
-	cout << "9 - Показать возможные операции" << endl;
-	cout << "0 - Выйти из программы" << endl;
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	string s;
+	HugeNumbers number;
+	int value = 0;
+
+	cout << "Введите число" << endl;
+	cin >> s;
+	cout << "======================================" << endl;
+
+	cout << "Возможные операции: " << endl;
+	cout << "1 - Вывод числа в консоль " << endl;
+	cout << "2 - Определить знак числа " << endl;
+	cout << "3 - Вывод суммы двух чисел " << endl;
+	cout << "4 - Вывод разности двух чисел " << endl;
+	cout << "9 - Просмотр операций " << endl;
+	cout << "0 - Завершить программу " << endl;
 	cout << "======================================" << endl;
 	
-	cout << "Введите первое число: " << endl;
-	cin >> number1;
-	cout << "Введите второе число: " << endl;
-	cin >> number2;
-
-	cout << "Определяется знак первого числа ";
-	result = numbers.foundSign1(number1); //проверить через if result
-	if (result == 1) {
-		cout << "Число является положительным " << endl;
-		cout << "======================================" << endl;
-	}
-	else {
-		cout << "Число является отрицательным " << endl;
-		cout << "======================================" << endl;
-	}
-
-	cout << "Определяется знак второго числа ";
-	result = numbers.foundSign2(number2);
-	if (result == 1) {
-		cout << "Число является положительным " << endl;
-		cout << "======================================" << endl;
-	}
-	else
-	{
-		cout << "Число является отрицательным " << endl;
-		cout << "======================================" << endl;
-	}
-
-
-
 	while (cin >> value) {
-		if (value == 1) 
+		if (value == 1) //ничего не выводит
 		{
-			cout << "Сумма чисел равна === " << numbers.getSum(number1, number2) << endl;
+			cout << "Введенное число === ";
+			number.showNumber(s);
 
 		}
-		else if (value == 2)
+		else if (value == 2) //done
 		{
-			cout << "Разность чисел равна === " << numbers.getSubtraction(number1, number2) << endl;
+			bool result;
+			cout << "Знак числа === ";
+			result = number.getSign(s);
+			if (result) {
+				cout << "positive" << endl;
+			}
+			else {
+				cout << "negative" << endl;
+			}
+
 		}
+		else if (value == 3) {
+			string s2;
+			cout << "Введите второе слагамое ";
+			cin >> s2;
+			cout << endl;
+			cout << "Сумма двух чисел равна === ";
+			number.Add(s2);
+		}
+
+		else if (value == 4) {
+
+		}
+
 		else if (value == 9)
 		{
-			cout << "Операции, которые можно совершать: " << endl;
-			cout << "1 - Вычислить сумму чисел  " << endl;
-			cout << "2 - Вычислить разность чисел" << endl;
-			cout << "9 - Показать возможные операции" << endl;
-			cout << "0 - Выйти из программы" << endl;
+			cout << "Возможные операции: " << endl;
+			cout << "1 - Вывод числа в консоль " << endl;
+			cout << "2 - Определить знак числа " << endl;
+			cout << "3 - Вывод суммы двух чисел " << endl;
+			cout << "4 - Вывод разности двух чисел " << endl;
+			cout << "9 - Просмотр операций " << endl;
+			cout << "0 - Завершить программу " << endl;
 			cout << "======================================" << endl;
 		}
 
@@ -98,6 +172,7 @@ int main()
 		cout << "======================================" << endl;
 	}
 
-	
-	return 0;
+
+
+return 0;
 }
